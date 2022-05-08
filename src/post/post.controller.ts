923bff56554ed4fa10c895fb592c031ce24821c0
Edwardz43 <b93303015@gmail.com>
2022-05-08 08:56:55 +0800

    add: find posts by author method

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,24 +1,31 @@
 import {
   Body,
   Controller,
+  Get,
   OnModuleInit,
+  Param,
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { CreatePostRequestDto, CreatePostResponseDto } from './post.dto';
+import {
+  CreatePostRequestDto,
+  CreatePostResponseDto,
+  FindByAuthorRequestDto,
+  FindByAuthorResponseDto,
+} from './post.dto';
 import { Client, ClientGrpc } from '@nestjs/microservices';
 import { grpcClientOptions } from '../grpc-client.options';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { lastValueFrom, Observable } from 'rxjs';
 
 interface PostService {
   create(post: CreatePostRequestDto): CreatePostResponseDto;
+  findByAuthor(authorId: FindByAuthorRequestDto): FindByAuthorResponseDto;
 }
 
-@UseGuards(AuthGuard())
-@ApiBearerAuth('jwt')
 @Controller('post')
-export class PostController implements OnModuleInit, PostService {
+export class PostController implements OnModuleInit {
   @Client(grpcClientOptions)
   private readonly client: ClientGrpc;
   private postService: PostService;
@@ -26,8 +33,19 @@ export class PostController implements OnModuleInit, PostService {
   onModuleInit() {
     this.postService = this.client.getService<PostService>('PostService');
   }
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth('jwt')
   @Post()
   create(@Body() post: CreatePostRequestDto): CreatePostResponseDto {
     return this.postService.create(post);
+  }
+
+  @Get('author/:authorId')
+  findByAuthor(
+    @Param() query: FindByAuthorRequestDto,
+  ): FindByAuthorResponseDto {
+    return this.postService.findByAuthor({
+      authorId: query.authorId,
+    });
   }
 }
--- src/post/post.dto.ts
+++ src/post/post.dto.ts
@@ -1,5 +1,54 @@
 import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 
+export class PostDto {
+  @ApiModelProperty({
+    description: 'Post ID',
+    required: true,
+    type: Number,
+    example: 1,
+  })
+  id: number;
+  @ApiModelProperty({
+    description: 'Post title',
+    required: true,
+    type: String,
+    example: 'My first post',
+  })
+  title: string;
+  @ApiModelProperty({
+    description: 'Post content',
+    type: String,
+    example: 'Long time ago in a galaxy far far away...',
+  })
+  content?: string;
+  @ApiModelProperty({
+    description: 'Author Name',
+    type: String,
+    example: 'somebody',
+  })
+  author?: string;
+  @ApiModelProperty({
+    description: 'Author ID',
+    required: true,
+    type: Number,
+    example: 1,
+  })
+  authorId: number;
+  @ApiModelProperty({
+    description: 'Post creation date',
+    required: true,
+    type: Date,
+    example: '2022-01-01 12:34:56',
+  })
+  createdAt: Date;
+  @ApiModelProperty({
+    description: 'Post last update date',
+    required: true,
+    example: '2022-01-02 12:34:56',
+  })
+  updatedAt: Date;
+}
+
 export class CreatePostRequestDto {
   @ApiModelProperty({
     description: 'Title of the post',
@@ -37,4 +86,21 @@ export class CreatePostResponseDto {
     type: String,
   })
   message: string;
+}
+
+export class FindByAuthorRequestDto {
+  @ApiModelProperty({
+    description: 'Author ID',
+    required: true,
+    type: Number,
+    example: 1,
+  })
+  authorId: number;
+}
+
+export class FindByAuthorResponseDto {
+  @ApiModelProperty({
+    description: 'Post List',
+  })
+  postList: PostDto;
 }
